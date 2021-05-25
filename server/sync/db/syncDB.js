4499555f956ad3a713429bac92f61ec20452d466
brockclayton <brock@formloco.com>
2021-05-24 21:07:05 -0600

    branding update

--- server/sync/db/syncDB.js
+++ server/sync/db/syncDB.js
@@ -23,12 +23,10 @@ const poolTenant = new Pool({
  * sync does not update form PIN, that is done in the form microservice
  */
 const formSyncSQL = async (data) => {
-
   poolTenant.options.database = data.user.tenant_id
   let client = await poolTenant.connect()
 
   for (let j = 0; j < data.forms.length; j++) {
-   
     let formObj = await client.query(`SELECT * FROM form WHERE form_id = '` + data.forms[j].form_id + `'`)
 
     if (formObj.rowCount === 0) {
@@ -40,38 +38,28 @@ const formSyncSQL = async (data) => {
       let formJSON = JSON.stringify(data.forms[j].form)
       await client.query(`UPDATE form SET form = '` + formJSON + `' WHERE form_id = '` + data.forms[j].form_id + `'`)
     }
-
   }
-
   let forms = await client.query(`SELECT * FROM form`)
-
   client.release()
-
   return forms.rows
 }
 
 const importSyncSQL = async (data) => {
-
   poolTenant.options.database = data.tenant_id
-  let client = await poolTenant.connect()
 
+  let client = await poolTenant.connect()
   let formJSON = JSON.stringify(data["form"])
   let formUser = JSON.stringify(data["user_created"])
 
   await client.query(`INSERT INTO form(form_id, form, pin, tenant_id, is_published, is_data, user_created) VALUES ( '` + data["form_id"] + `', '` + formJSON + `', '` + data["pin"] + `', '` + data["tenant_id"] + `', ` + data["is_published"] + `, ` + data["is_data"] + `, '` + formUser + `')`)
-
   client.release()
-
 }
 
 /** insert indexedDB and file upload data to db */
 const dataSyncToTenantSQL = async (data) => {
-
   for (let j = 0; j < data.length; j++) {
-
     poolTenant.options.database = data[j]["tenant_id"]
     let client = await poolTenant.connect()
-
     /** id conflict: id is auto generated on indexDb and postgresq
      * form_id and tenant_id are redundant,form_columns is used
      * to create data table if not exist on sync
@@ -126,13 +114,10 @@ const dataSyncToTenantSQL = async (data) => {
     }
 
     let valStr = valueStr.slice(0, -1) // pull last comma
-
     let columns = data[j].form_columns.replace(/`/g, "'")
 
     if (data[j].file_array !== undefined) {
-
       for (let i = 0; i < data[j].file_array.length; i++) {
-
         await client.query(`INSERT INTO public.files (tenant_id, form_id, file_name, type, user_created) VALUES ('` + data[j]["tenant_id"] + `', '` + data[j]["form_id"] + `', '` + data[j]["file_array"]["name"] + `', '` + data[j]["file_array"]["type"] + `', '` + data[j]["user_created"] + `')`)
 
         if (!fs.existsSync(staticPath + 'files/' + data[j]["tenant_id"])) {
@@ -147,22 +132,16 @@ const dataSyncToTenantSQL = async (data) => {
           })
         }
       }
-
     }
-
     let dataColumns = keyArray.toString()
 
     // create data table
     await client.query(`CREATE SEQUENCE IF NOT EXISTS id_seq`)
-
     await client.query(`CREATE TABLE IF NOT EXISTS "` + data[j].form_id + `" (` + columns + `)`)
-
     await client.query(`INSERT INTO "` + data[j].form_id + `" (` + dataColumns + `) VALUES (` + valStr + `)`)
 
     client.release()
-
   }
-
 }
 
 /**
@@ -171,41 +150,29 @@ const dataSyncToTenantSQL = async (data) => {
  * if no data from indexedDB just return all tenant list data
  */
 const dataListTenantSyncSQL = async (listObj) => {
-
   let client
-
   for (let i = 0; i < listObj.data.length; i++) {
-
     if (listObj["tenant_id"] === listObj.data[i]["tenant_id"]) {
-
       poolTenant.options.database = listObj.data[i]["tenant_id"]
       let client = await poolTenant.connect()
-
       let columns = listObj.data[i].columns.replace(/`/g, "'")
 
       await client.query(`CREATE SEQUENCE IF NOT EXISTS id_seq`)
-
       await client.query(`CREATE TABLE IF NOT EXISTS "` + listObj.data[i]["form_id"] + `" (` + columns + `)`)
-
       await client.query(`DELETE FROM "` + listObj.data[i]["form_id"] + `"`)
 
       client.release()
-
     }
-
   }
 
 
   for (let j = 0; j < listObj.data.length; j++) {
-
     if (listObj["tenant_id"] === listObj.data[j]["tenant_id"]) {
-
       poolTenant.options.database = listObj["tenant_id"]
       client = await poolTenant.connect()
 
       let keyArray = Object.keys(listObj.data[j])
       let valueArray = Object.values(listObj.data[j])
-
       let idx = keyArray.indexOf('id')
       if (idx !== -1) {
         keyArray.splice(idx, 1)
@@ -214,7 +181,6 @@ const dataListTenantSyncSQL = async (listObj) => {
 
       let idxData = keyArray.indexOf('data')
       let idxUserCreated = keyArray.indexOf('user_created')
-
       let valueStr = ``
       for (let i = 0; i < valueArray.length; i++) {
         if (idxData === i)
@@ -228,15 +194,11 @@ const dataListTenantSyncSQL = async (listObj) => {
       }
 
       let valStr = valueStr.slice(0, -1) // pull last comma
-
       let dataColumns = keyArray.toString()
-
       await client.query(`INSERT INTO "` + listObj.data[j].form_id + `" (` + dataColumns + `) VALUES (` + valStr + `)`)
 
       client.release()
-
     }
-
   }
 
   poolTenant.options.database = listObj["tenant_id"]
@@ -247,11 +209,8 @@ const dataListTenantSyncSQL = async (listObj) => {
   let listData = []
   let lastFormID
   for (let j = 0; j < allForms.rows.length; j++) {
-
     if (allForms.rows[j]["form"]["is_list"]) {
-
       let data = await client.query(`SELECT * FROM "` + allForms.rows[j]["form_id"] + `"`)
-
       let obj = {
         columns: data.rows[0].columns,
         data: data.rows[0].data,
@@ -260,13 +219,9 @@ const dataListTenantSyncSQL = async (listObj) => {
       }
       listData.push(obj)
     }
-
   }
-
   client.release()
-
   return listData
-
 }
 
 /**
@@ -274,11 +229,9 @@ const dataListTenantSyncSQL = async (listObj) => {
  * used when temporary token is issued - 'copy link', 'embedded' forms
  */
 const dataListFormSyncSQL = async (data) => {
-
   let listArray = []
   let lastFormID
   for (let j = 0; j < data.length; j++) {
-
     poolTenant.options.database = data[j]["tenant_id"]
     let client = await poolTenant.connect()
 
@@ -288,30 +241,21 @@ const dataListFormSyncSQL = async (data) => {
       listArray = listArray.concat(listData.rows)
       lastFormID = data[j].form_id
     }
-
     client.release()
-
   }
-
   return listArray
-
 }
 
 const listsTenantSQL = async (listObj) => {
-
   poolTenant.options.database = listObj["tenant_id"]
   client = await poolTenant.connect()
 
   let allForms = await client.query(`SELECT * FROM form WHERE date_archived is null`)
-
   let listData = []
   let lastFormID
   for (let j = 0; j < allForms.rows.length; j++) {
-
     if (allForms.rows[j]["form"]["is_list"]) {
-
       let data = await client.query(`SELECT * FROM "` + allForms.rows[j]["form_id"] + `"`)
-
       let obj = {
         columns: data.rows[0].columns,
         data: data.rows[0].data,
@@ -320,16 +264,12 @@ const listsTenantSQL = async (listObj) => {
       }
       listData.push(obj)
     }
-
     client.release()
-
   }
-
   return listArray
 }
 
 const syncDeleteListSQL = async (formObj) => {
-
   poolTenant.options.database = formObj["tenant_id"]
   let client = await poolTenant.connect()
 
@@ -337,7 +277,6 @@ const syncDeleteListSQL = async (formObj) => {
   await client.query(`DELETE FROM form WHERE form_id = '` + formObj["form_id"] + `'`)
 
   client.release()
-
 }
 
 module.exports = {
