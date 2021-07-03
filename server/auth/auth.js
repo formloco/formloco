d1f6e2e0e44b5be7fdaaf2fddb64031c5d1c5475
brockclayton <brock@formloco.com>
2021-07-03 13:15:06 -0600

    bump

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -6,8 +6,16 @@ const loadConfig = require('../config')
 loadConfig()
 
 app.use(express.json())
-app.options('*', cors())
+
+let corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
 app.use(cors())
+app.options('*', cors())
+
+app.use(cors(corsOptions))
 app.use(routes)
  
 app.listen(9000, () => console.log('auth api listening on port 9000!'))
